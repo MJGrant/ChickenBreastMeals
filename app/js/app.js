e78336c3e4b2d093e3eca1350a36636172974be9
Mandi Grant <mandi018@gmail.com>
2014-07-20 17:15:55 -0700

    Starting work on the recipe editor CMS. Not functional, just structural right now.

--- app/js/app.js
+++ app/js/app.js
@@ -12,4 +12,5 @@ config(['$routeProvider', function($routeProvider) {
   	$routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'MealListCtrl'});
   	$routeProvider.when('/tools', {templateUrl: 'partials/tools.html', controller: 'MealListCtrl'});
   	$routeProvider.when('/privacy-disclosures', {templateUrl: 'partials/privacy-disclosures.html', controller: 'MealListCtrl'});
+  	$routeProvider.when('/cms', {templateUrl: 'admin/cms.html', controller: 'MealListCtrl'});
 }]);
