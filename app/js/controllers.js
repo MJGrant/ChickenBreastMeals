bed4c81d9b0a5a6bcab56732a7b9aad41fbfa61f
Mandi Grant <mandi018@gmail.com>
2014-07-26 19:36:28 -0700

    Starting the transition to a mongoDB backed data solution. Transitioning off of using json files. This commit breaks the build for now, but represents a significant step forward.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,16 +3,41 @@
 /* Controllers */
 var cbmAppControllers = angular.module('cbmAppControllers', []);
 
-cbmAppControllers.controller('MealListCtrl', ['$scope', '$http',
-	function($scope, $http) {
-	$http.get('data/meals.json').success(function(data) {
-		$scope.meals = data;
-	});
+cbmAppControllers.controller('MasterCtrl', function($scope, $http) {
+	$scope.getMeals = function() {
+		$http.get('/api/db').success(function(data) {
+			$scope.meals = data;
+		})
+		.error(function(data) {
+			console.log("getMeals Error: " + data);
+		});
+	};
 
+	$scope.createNewMeal = function(newMeal) {
+		$http.post('/api/db',newMeal)
+		.success(function(data) {
+			$scope.meals = data;
+			console.log("post success: " + data.title);
+		})
+		.error(function(data) {
+			console.log("error");
+		});
+	};
+});
+
+cbmAppControllers.controller('AdminCtrl', ['$scope', function($scope) {
+	$scope.test = 'test';
+	$scope.postMeal = function() {
+		$scope.createNewMeal($scope.mealDetail);
+	};
+}]);
+
+cbmAppControllers.controller('MealListCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.getMeals();
 	$scope.viewMealDetail = function(id) {
 		$http.get('data/' + id + '.json').success(function(data) {
 			$scope.mealDetail = data;
-		}).error(function(data, status, headers, config) { 
+		}).error(function(data, status, headers, config) {
 			alert("Error loading recipe." + data);
 		});
 	};
@@ -34,6 +59,7 @@ cbmAppControllers.controller('DetailsController', ['$scope', function($scope) {
 	{ name: 'partials/test1.html', url: 'partials/test1.html'} ];
 	$scope.template = $scope.templates[0];
 }]);
+
 
 cbmAppControllers.controller('MealDetailsCtrl', ['$scope', '$routeParams', '$http',
 	function($scope, $routeParams, $http) { }
---
+++ server.js
@@ -0,0 +1,89 @@
+/*
+var express = require('express');
+var app = express();
+var port = process.env.port || 8000;
+
+app.get('/sample', function(req, res) {
+	res.send('this is a sample!');
+});
+
+app.listen(port);
+console.log('Now listening on port ' + port);
+*/
+
+
+
+//things you want executed and available for the server
+//setup
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+//config
+app.use(express.static(__dirname + '/app')); //set the app directory
+app.use(morgan('combined')); //logs server requests as they're made
+app.use(bodyParser());
+
+//connect and define database schema
+mongoose.connect('mongodb://localhost/cbm_database');
+var db = mongoose.model('Meals', {
+	title:String,
+	snippet:String,
+	cooktemp:Number,
+	ingredients:[],
+	instructions:[],
+	images:[],
+	dietary:{dairyfree: Boolean, glutenfree: Boolean, lowcarb: Boolean}
+});
+
+app.route('/login')
+	.get(function(req, res) {
+		res.send('this is the login form');
+	});
+
+app.route('/api/db/')
+	.get(function(req,res) { //request, response
+		console.log("calling get");
+		db.find(function(err,meals) { //error if there is one, data that comes back
+			if (err) {
+				res.send(err);
+			}
+			res.json(meals);
+		});
+	})
+	.post(function(req,res){
+		console.log("calling post");
+		var m = req.body; //some express thing
+		db.create({
+			title:m.title,
+			snippet:m.snippet,
+			cooktemp:m.cooktemp,
+			ingredients:m.ingredients,
+			instructions:m.instructions,
+			images:m.images,
+			dietary:{dairyfree:m.dairyfree, glutenfree:m.glutenfree, lowcarb:m.lowcarb}
+		},function(err,newMeal) {
+			if (err) {
+				res.send(err);
+			}
+			db.find(function(err, meals) {
+				if (err) {
+					res.send(err);
+				}
+			res.json(meals);
+			});
+		});
+	});
+
+app.route('*') //catch all
+	.get(function(req,res) {
+		res.sendfile('./app/index.html');
+	});
+
+//listen - start app with node server.js
+var port = Number(process.env.PORT || 8000 );
+app.listen(port, function() {
+	console.log("CBM Listening on " + port);
+});
